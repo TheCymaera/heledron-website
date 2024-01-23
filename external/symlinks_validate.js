fc49871a74f20fc82531d7b9dd81945b030cdb92
TheCymaera <69355340+TheCymaera@users.noreply.github.com>
2024-01-24 06:03:10 +0800

    ✨ Validate repositories

--- external/symlinks_validate.js
+++ external/symlinks_validate.js
@@ -1,27 +1,64 @@
-// deno run --allow-read --allow-write createSymlinks.js
-
-import { symlinks, inputFolder, outputFolder } from "./symlinks_config.js";
+import { symlinks, inputFolder } from "./symlinks_config.js";
 import { exec, OutputMode } from "https://deno.land/x/exec/mod.ts";
 
-// Deno create symlink
-const maxLength = Math.max(...Object.keys(symlinks).map(s => s.length));
+// get flag from command line
+const doFetch = Deno.args.includes("--fetch");
 
-for (const [outputPath, inputPath] of Object.entries(symlinks)) {
+console.log(`Validating repositories... (fetch: ${doFetch})`);
+
+for (const [_, inputPath] of Object.entries(symlinks)) {
+	scanRepo(inputPath, doFetch).then(result => {
+		if (!result) return;
+
+		const { gitIsUpToDate, noUnStagedChanges, buildIsUpToDate } = result;
+
+		if (gitIsUpToDate && noUnStagedChanges && buildIsUpToDate) {
+			console.log(inputPath + ": " + "✅");
+			return;
+		}
+
+		console.log(inputPath + ": ");
+		console.log(" ".repeat(4) + "git: " + (gitIsUpToDate ? "✅" : "❌"));
+		console.log(" ".repeat(4) + "unstaged: " + (noUnStagedChanges ? "✅" : "❌"));
+		console.log(" ".repeat(4) + "build: " + (buildIsUpToDate ? "✅" : "❌"));
+		if (!buildIsUpToDate) {
+			console.log(" ".repeat(4 * 2) + "last build: " + result.lastBuild);
+			console.log(" ".repeat(4 * 2) + "last commit: " + result.lastCommit);
+		}
+	});
+}
+
+async function scanRepo(inputPath, fetch = false) {
 	try {
-		const inputRepository = Deno.realPathSync(inputFolder + inputPath + "/../");
+		const repositoryPath = Deno.realPathSync(inputFolder + inputPath + "/../");
+		const buildPath = Deno.realPathSync(inputFolder + inputPath);
 
+		// cd
+		Deno.chdir(repositoryPath);
 
-		// cd into repository, show an error if repository is not on the latest version using "git status"
-		Deno.chdir(inputRepository);
+		// fetch updates
+		if (fetch) await exec("git fetch");
+
 		const gitStatus = await exec("git status", { output: OutputMode.Capture });
 
-		const isUpToDate = gitStatus.output.includes("Your branch is up to date with 'origin/master'");
-		const hasUnStagedChanges = gitStatus.output.includes("Changes not staged for commit");
+		const gitIsUpToDate = gitStatus.output.includes("Your branch is up to date with 'origin/master'");
+		const noUnStagedChanges = !gitStatus.output.includes("Changes not staged for commit");
 
-		console.log(inputPath.padEnd(maxLength) + ": " + (isUpToDate ? "up to date" : "not up to date") +  " | " + (hasUnStagedChanges ? "has unstaged changes" : ""));
+		const lastCommitOutput = await exec("git log -1 --format=%cd", { output: OutputMode.Capture });
+		const lastCommit = new Date(lastCommitOutput.output);
+		const lastBuild = Deno.statSync(buildPath).mtime;
+
+		const buildIsUpToDate = lastCommit <= lastBuild;
+
+		return {
+			gitIsUpToDate,
+			noUnStagedChanges,
+			buildIsUpToDate,
+			lastBuild,
+			lastCommit,
+		};
 	}
 	catch (error) {
-		console.error(error + "\n" + inputFolder + inputPath + "\n -> " + outputFolder + outputPath);
-		continue;
+		console.error(error);
 	}
 }
