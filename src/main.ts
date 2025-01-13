e128aad885724e89be5d1351c4da0836426fa0e0
Cymaera <69355340+TheCymaera@users.noreply.github.com>
2025-01-13 18:35:40 +0800

    Fix icons

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,22 @@ import "./layout.css";
 import "./skin.css";
 import { fa5_brands_discord, fa5_brands_github, fa5_brands_instagram, fa5_brands_patreon, fa5_brands_redditAlien, fa5_brands_tiktok, fa5_brands_twitter, fa5_brands_youtube, fa5_solid_envelope, fa6_brands_bluesky } from "fontawesome-svgs";
 
-document.querySelector(".fa5_brands_twitter")!.innerHTML = fa5_brands_twitter;
-document.querySelector(".fa5_brands_github")!.innerHTML = fa5_brands_github;
-document.querySelector(".fa5_brands_redditAlien")!.innerHTML = fa5_brands_redditAlien;
-document.querySelector(".fa5_brands_youtube")!.innerHTML = fa5_brands_youtube;
-document.querySelector(".fa5_brands_discord")!.innerHTML = fa5_brands_discord;
-document.querySelector(".fa5_brands_envelope")!.innerHTML = fa5_solid_envelope;
-document.querySelector(".fa5_brands_patreon")!.innerHTML = fa5_brands_patreon;
-document.querySelector(".fa5_brands_bluesky")!.innerHTML = fa6_brands_bluesky;
-document.querySelector(".fa5_brands_instagram")!.innerHTML = fa5_brands_instagram;
-document.querySelector(".fa5_brands_tiktok")!.innerHTML = fa5_brands_tiktok;
+const setHTML = (selector: string, html: string) => {
+	const element = document.querySelector(selector);
+	if (!element) return;
+	element.innerHTML = html;
+}
+
+setHTML(".fa5_brands_twitter", fa5_brands_twitter);
+setHTML(".fa5_brands_github", fa5_brands_github);
+setHTML(".fa5_brands_redditAlien", fa5_brands_redditAlien);
+setHTML(".fa5_brands_youtube", fa5_brands_youtube);
+setHTML(".fa5_brands_discord", fa5_brands_discord);
+setHTML(".fa5_brands_envelope", fa5_solid_envelope);
+setHTML(".fa5_brands_patreon", fa5_brands_patreon);
+setHTML(".fa5_brands_bluesky", fa6_brands_bluesky);
+setHTML(".fa5_brands_instagram", fa5_brands_instagram);
+setHTML(".fa5_brands_tiktok", fa5_brands_tiktok);
 
 // if (matchMedia('(prefers-color-scheme: dark)').matches) {
 // 	document.documentElement.dataset.theme = 'dark';
